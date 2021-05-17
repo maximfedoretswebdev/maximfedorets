34c48106df237c4d931ede78d959bf0b9355a191
maximfedoretswebdev <69167344+maximfedoretswebdev@users.noreply.github.com>
2021-05-16 18:25:31 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -20,3 +20,29 @@ const countdown = () => {
 };
 
 setInterval(countdown, 1000);
+
+
+
+function clock() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+
+
+
+if (hourNow > 18) {
+    greeting = "Good afternoon";
+}else if (hourNow > 12) {
+    greeting = "Good evening";
+}else if (hourNow > 0) {
+    greeting = "Good morning";
+}else {
+    greeting = "Welcome!";
+}
+
+document.querySelector(".greeting_txt").innerHTML = greeting;
+
+};
+
+clock();
+
