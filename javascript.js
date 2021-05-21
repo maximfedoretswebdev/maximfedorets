04d4814d40ad1f03b3b255a3178b8dfda973904b
maximfedoretswebdev <69167344+maximfedoretswebdev@users.noreply.github.com>
2021-05-21 13:55:30 -0700

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,25 +1,25 @@
-const countdown = () => {
-    const countDate = new Date('May 22, 2021 00:00:00').getTime();
-    const now = new Date().getTime();
-    const gap = countDate - now;
+// const countdown = () => {
+//     const countDate = new Date('May 22, 2021 00:00:00').getTime();
+//     const now = new Date().getTime();
+//     const gap = countDate - now;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24; 
+//     const second = 1000;
+//     const minute = second * 60;
+//     const hour = minute * 60;
+//     const day = hour * 24; 
 
-    const textDay = Math.floor(gap / day);
-    const textHour = Math.floor((gap % day) / hour);
-    const textMinute = Math.floor((gap % hour) / minute);
-    const textSecond = Math.floor((gap % minute) / second);
+//     const textDay = Math.floor(gap / day);
+//     const textHour = Math.floor((gap % day) / hour);
+//     const textMinute = Math.floor((gap % hour) / minute);
+//     const textSecond = Math.floor((gap % minute) / second);
 
-    document.querySelector(".day").innerHTML = textDay;
-    document.querySelector(".hour").innerHTML = textHour;
-    document.querySelector(".minute").innerHTML = textMinute;
-    document.querySelector(".second").innerHTML = textSecond;
-};
+//     document.querySelector(".day").innerHTML = textDay;
+//     document.querySelector(".hour").innerHTML = textHour;
+//     document.querySelector(".minute").innerHTML = textMinute;
+//     document.querySelector(".second").innerHTML = textSecond;
+// };
 
-setInterval(countdown, 1000);
+// setInterval(countdown, 1000);
 
 
 
@@ -46,3 +46,9 @@ document.querySelector(".greeting_txt").innerHTML = greeting;
 
 clock();
 
+
+
+
+function underwork(){
+    alert("Sorry this section is still under work, plese come back later.")
+}
