d428f733984de058784080e7ffd71437e232e7eb
maximfedoretswebdev <69167344+maximfedoretswebdev@users.noreply.github.com>
2021-08-23 15:11:32 -0700

    Add files via upload

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,45 +1,52 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyABPkrQM3W4M7X47duNwvzLOx_aHn6qm5M",
-    authDomain: "maxwebdevlogin.firebaseapp.com",
-    projectId: "maxwebdevlogin",
-    storageBucket: "maxwebdevlogin.appspot.com",
-    messagingSenderId: "425661031757",
-    appId: "1:425661031757:web:d06258533f4b685601c304"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDNygIcFGlAKbxT0QenZnOjGhxsj_V6WGM",
+    authDomain: "maxweb-dev.firebaseapp.com",
+    projectId: "maxweb-dev",
+    storageBucket: "maxweb-dev.appspot.com",
+    messagingSenderId: "820857592230",
+    appId: "1:820857592230:web:49834065d97d3621c19767",
+    measurementId: "G-H3VKVGRBZ2"
   };
 
-  // Initialize Firebase
+    // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
     const auth = firebase.auth();
 
-// This is the Log in function
-function signIn(){
+function signUp(){
+            
     var email = document.getElementById("email");
     var password = document.getElementById("password");
-    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+            
+    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
     promise.catch(e => alert(e.message));
-    
-    window.location.href = "visits.html";
+            
+    alert(email.value + " has signed up");
 }
+        
+        
+function signIn(){   
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+            
+    var promise = auth.signInWithEmailAndPassword(email.value, password.value);
+    promise.catch(e => alert(e.message));
 
-// Log out fuction
+}     
+    
+
 function signOut(){
     auth.signOut();
-    window.location.href = "login.html";
 }
 
+firebase.auth().onAuthStateChanged((user) => {
+    if(user){
+        var email = user.email;
+        alert("Active user " + email)
+    } else {
+        alert("No active users")
+    }
+  });
 
 
-
-
-    auth.onAuthStateChanged(function(user){
-		if(user){
-			// var email = user.email;
-			var personalEml =  document.getElementById("personalEmail");
-            personalEml.innerHTML = `<div>${user.email}</div>`;
-			//Take user to a different or home page
-			//is signed in
-		}else{
-			//no user is signed in
-		}
-    });
